38e52ef6b90e577485c8a4cb9c6339b2818323a2
maya <148207189+06maayan@users.noreply.github.com>
2023-10-26 18:38:28 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,12 @@
+let userName;
+userName = "体重";
+weight = 68;
+console.log(userName);
+console.log(weight);
+
+userName = "身長";
+height = 1.7;
+console.log(userName);
+console.log(height);
+
+console.log((weight) /( height * height));
