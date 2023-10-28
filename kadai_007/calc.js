0a6e6caf841b539b960188847d2b5c093565be72
maya <148207189+06maayan@users.noreply.github.com>
2023-10-28 16:53:02 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,18 @@
+let num = Math.floor(Math.random() * 4);
+console.log(num);
+
+if (num === 3) {
+    console.log("3倍です。");
+}
+
+else if (num === 2) {
+    console.log("5倍です。");
+}
+
+else if (num === 1) {
+    console.log("3と5倍です。");
+}
+
+else{
+    console.log("その他")
+}
